1612d8611d31eecb0270fb2e554572f8f33da3be
ExpertOfAI <70627925+ExpertOfAI@users.noreply.github.com>
2024-09-09 16:08:15 +0530

    Add files via upload

--- power_nlp/power.py
+++ power_nlp/power.py
@@ -1,11 +1,6 @@
 from collections import defaultdict
-from nltk.util import ngrams
-from nltk.corpus import wordnet
 import re
 
-def g_cbow(word, loaders):
-	return loaders.cbow[word] if word in loaders.cbow else set()
-	
 def tfidf(id_terms, cat_product_count_filtered):	
 	term_max_freq = defaultdict(int)
 	for terms_count in id_terms.values():
@@ -39,98 +34,6 @@ def create_cbow(docs):
 		for word in doc:
 			indexes[word].add(_id)	
 	return indexes
-	
-def similarity_knn(combs, loaders):
-	matched = {}
-	ordered = {}
-	if len(combs[0])>1:
-		for combination in combs:			
-			lst = [g_cbow(c,loaders) for c in combination]
-			lst.sort(key = len, reverse=True)
-			smallest_set = lst.pop()
-			combinations_matched_temp = len(smallest_set.intersection(*lst))
-			if combinations_matched_temp > 0:
-				pairs = [" ".join(sorted(n)) for n in ngrams(combination, 2)]		
-				matched[tuple(combination)] = combinations_matched_temp
-				ordered[tuple(combination)] = sum([loaders.bigrams_ordered[n] for n in pairs if n in loaders.bigrams_ordered])	
-		if len(matched)>0 and len(ordered)>0:
-			matched_max = max(matched.values())
-			ordered_max = max(ordered.values())
-			if matched_max > 0 and ordered_max > 0: 
-				matched = {comb:round(50*score/matched_max,2) for comb,score in matched.items()}
-				ordered = {comb:round(50*score/ordered_max,2) for comb,score in ordered.items()}		
-				matched = {comb:score+ordered[comb] for comb,score in matched.items()}
-	else:
-		matched = {tuple(combination):len(g_cbow(combination[0],loaders)) for combination in combs}
-	if matched:
-		most_frequent = max(matched, key= lambda x: matched[x])
-		return list(most_frequent)	   
-	else:
-		return list()
-		
-def lemma_excluding_brand(word, loaders):
-	original_word = word	
-	if len(word) > 3 and word not in loaders.brand_mapping:
-		number = ""
-		seg = re.compile("(^\d+\.*\d*)([a-z]+$)").match(word)
-		if seg:
-			number = seg.groups()[0]
-			word = seg.groups()[1]
-		for tag in loaders.morphy_pos_tags:
-			word = wordnet.morphy(word,tag) or word
-		temp = re.sub('s$', '', word)
-		if len(word)>2 and temp[-1]!="s":
-			word = temp
-		word = number+word
-		if word in loaders.brand_mapping:
-			word = original_word
-	return word
-def lemma(word, loaders):
-	original_word = word	
-	if len(word) > 3:
-		number = ""
-		seg = re.compile("(^\d+\.*\d*)([a-z]+$)").match(word)
-		if seg:
-			number = seg.groups()[0]
-			word = seg.groups()[1]
-		for tag in loaders.morphy_pos_tags:
-			word = wordnet.morphy(word,tag) or word
-		temp = re.sub('s$', '', word)
-		if len(word)>2 and temp[-1]!="s":
-			word = temp
-		word = number+word
-	return word
-def lemma_excluding_brandcategory(word, loaders):
-	original_word = word
-	if len(word) > 3 and word not in loaders.category_brand_words_list:
-		number = ""
-		seg = re.compile("(^\d+\.*\d*)([a-z]+$)").match(word)
-		if seg:
-			number = seg.groups()[0]
-			word = seg.groups()[1]
-		for tag in loaders.morphy_pos_tags:
-			word = wordnet.morphy(word,tag) or word
-		temp = re.sub('s$', '', word)
-		if len(word)>2 and temp[-1]!="s":
-			word = temp
-		word = number+word
-		if word in loaders.category_brand_words_list:
-			word = original_word
-	return word
-	
-def clean_text(input_text, loaders):
-	text = input_text.lower() 
-	text = re.sub(r'\n', ' ', text) 
-	text = text.replace('..','.').replace('. ', ' ').replace("- "," ").replace(" -"," ")
-	text = re.sub(r'[,:;{}?!/_\$@<>()\\#%+=\[\]\']',' ', text)
-	text = re.sub(r'\d+\.*\d*\w*x\d+\.*\d*\w*', ' ', text, flags=re.IGNORECASE)		
-	text = re.sub(r'[^a-z0-9.*\- ]', '', text)
-	text = text.rstrip('.') 
-	text = " ".join([t.replace("."," ") if t.replace(".","").isalpha() else t for t in text.split()])
-	text = " ".join([t.replace("-"," ") if t.replace("-","").isalnum() else t for t in text.split()])
-	text = " ".join([t.replace("*","x") if t.replace(".","").replace("*","").isnumeric() else t.replace("*","") for t in text.split()])
-	text = ' '.join([t for t in text.split() if t not in loaders.stopwords])
-	return text
 	
 def clean_input(input_text):
 	text = " ".join(input_text)
